c891a7fb2ccedb2be57ee2236e91ae18f59be47e
zian song <36784023+song-zian@users.noreply.github.com>
2021-09-30 17:25:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,5 +21,13 @@ const readme = `\
 
 ![Progress Bar CI](https://github.com/song-zian/song-zian/workflows/Progress%20Bar%20CI/badge.svg)\
 `
+### My GitHub Contributions
+
+![](https://raw.githubusercontent.com/L1cardo/L1cardo/main/assets/github-contribution-grid-snake.svg)
+    
+![Licardo's GitHub stats](https://github-readme-stats.vercel.app/api?username=l1cardo&show_icons=true)
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=l1cardo&layout=compact)
+
+⭐️ From [L1cardo](https://github.com/L1cardo)
 
 console.log(readme)
